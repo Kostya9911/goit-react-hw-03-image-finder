96707a7860a4b0f41507b1c43eea7b34e20623f4
Kostya9911 <110036410+Kostya9911@users.noreply.github.com>
2023-09-03 23:39:19 +0200

    add change for modal css

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,7 +6,7 @@ export const Modal = ({ largeImageURL, closeModal }) => {
   return (
     <div onClick={closeModal} className={css.Overlay}>
       <div className={css.Modal}>
-        <img src={largeImageURL.largeImageURL} alt="" />
+        <img src={largeImageURL.largeImageURL} className={css.img} alt="" />
       </div>
     </div>
   );
